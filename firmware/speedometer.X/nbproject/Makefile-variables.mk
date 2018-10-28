aa59707d7510e3e9af6dd80fdf8df7c4981da181
Gabbla <zampierigabriele.ist@gmail.com>
2018-10-28 16:03:17 +0100

    Initial Commit.

---
+++ firmware/speedometer.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=speedometer.X.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/speedometer.X.production.hex
+CND_PACKAGE_DIR_Debug=${CND_DISTDIR}/Debug/package
+CND_PACKAGE_NAME_Debug=speedometer.x.tar
+CND_PACKAGE_PATH_Debug=${CND_DISTDIR}/Debug/package/speedometer.x.tar
