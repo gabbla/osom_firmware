d49dd3e986c9bb2cc1fb7d1b167088f3e92e196a
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-13 00:44:18 +0100

    added laser ifc

---
+++ firmware/src/laserIfc.h
@@ -0,0 +1,13 @@
+#ifndef LASERIFC_H_LKZSYRXB
+#define LASERIFC_H_LKZSYRXB
+
+#include "mailbox.h"
+
+#define LASER_DX    1
+#define LASER_SX    2
+
+void LaserIfc_TurnOn(const uint8_t LASER);
+void LaserIfc_TurnOff(const uint8_t LASER);
+
+
+#endif /* end of include guard: LASERIFC_H_LKZSYRXB */
