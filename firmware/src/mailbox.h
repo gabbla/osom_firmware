438dbfc15688186a7e9c8aff66d05d0a947d77ee
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-15 00:24:31 +0100

    sources added

--- firmware/src/mailbox.h
+++ firmware/src/mailbox.h
@@ -2,6 +2,10 @@
 #define MAILBOX_H_VY2CRLWE
 
 // Useful stuff about mailboxes
+// Sources
+#define MSG_SRC_BLE             (0)
+#define MSG_SRC_MAIN            (1)
+#define MSG_SRC_COM             (2)
 
 // LASER
 #define LASER_MAILBOX           (SYS_MSG_0)
@@ -11,6 +15,6 @@
 // LASER
 #define MAIN_MAILBOX           (SYS_MSG_0)
 #define MAIN_MSG_PRIORITY      (0)
-#define MAIN_MSG_ID            (0)
+#define MAIN_MSG_ID            (1)
 
 #endif /* end of include guard: MAILBOX_H_VY2CRLWE */
