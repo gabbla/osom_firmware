86224f371f0322abfdebd2bf4a6a48dda8521e24
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-24 22:57:46 +0100

    test message added, will be removed

--- firmware/src/mainapp.c
+++ firmware/src/mainapp.c
@@ -101,7 +101,27 @@ void MAINAPP_Tasks ( void )
                 Packet *p = (Packet*)next->pData;
                 DEBUG("Cmd: 0x%02X", p->cmd);
                 // TODO parse
-                PACKET_Free(next->pData); // really important
+                switch(p->cmd){
+                    case BLE_CMD_MODE: {
+                        Packet *reply = PACKET_Create();
+                        reply->msgID = p->msgID;
+                        reply->cmd = BLE_CMD_MODE;
+                        SYS_MSG_OBJECT message;
+                        message.nMessageTypeID = BLEOUT_MSG_ID;
+                        message.nSource = MSG_SRC_MAIN;
+                        message.nSizeData = sizeof(Packet);
+                        message.pData = (uintptr_t*)reply;
+                        SYS_MSG_RESULTS myRes = SYS_MSG_MessageSend(BLEOUT_MAILBOX, &message);
+                        if(myRes != SYS_MSG_SENT) {
+                            ERROR("Failed to send!! %d", myRes);
+                        } 
+                        break;
+                    }
+                    default:
+                        WARN("IDK what to do with %d", p->cmd);
+                        break;
+                }
+                PACKET_Free(p); // really important
             }
             break;
         }
