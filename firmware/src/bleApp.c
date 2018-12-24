c77f217aff01f0375e13d97299399ad4459a140a
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-24 16:17:20 +0100

    removed autogen comments

--- firmware/src/bleApp.c
+++ firmware/src/bleApp.c
@@ -1,89 +1,8 @@
-/*******************************************************************************
- MPLAB Harmony Application Source File
-
- Company:
- Microchip Technology Inc.
-
- File Name:
- bleapp.c
-
- Summary:
- This file contains the source code for the MPLAB Harmony application.
-
- Description:
- This file contains the source code for the MPLAB Harmony application.  It
- implements the logic of the application's state machine and it may call
- API routines of other MPLAB Harmony modules in the system, such as drivers,
- system services, and middleware.  However, it does not call any of the
- system interfaces (such as the "Initialize" and "Tasks" functions) of any of
- the modules in the system or make any assumptions about when those functions
- are called.  That is the responsibility of the configuration-specific system
- files.
- *******************************************************************************/
-
-// DOM-IGNORE-BEGIN
-/*******************************************************************************
- Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
-
- Microchip licenses to you the right to use, modify, copy and distribute
- Software only when embedded on a Microchip microcontroller or digital signal
- controller that is integrated into your product or third party product
- (pursuant to the sublicense terms in the accompanying license agreement).
-
- You should refer to the license agreement accompanying this Software for
- additional information regarding your rights and obligations.
-
- SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
- EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
- MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
- IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
- CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
- OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
- INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
- CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
- SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
- (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
- *******************************************************************************/
-// DOM-IGNORE-END
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files 
-// *****************************************************************************
-// *****************************************************************************
 #include "bleApp.h"
 #include "driver/tmr/src/drv_tmr_local.h"
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Global Data Definitions
-// *****************************************************************************
-// *****************************************************************************
-
-// *****************************************************************************
-/* Application Data
-
- Summary:
- Holds application data
-
- Description:
- This structure holds the application's data.
-
- Remarks:
- This structure should be initialized by the APP_Initialize function.
-
- Application strings and buffers are be defined outside this structure.
- */
-
 BLEAPP_DATA bleappData;
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Callback Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/* TODO:  Add any necessary callback functions.
- */
 void registerBuffer();
 void unregisterBuffer(); //Forward declaration
 void reregisterBuffer();
@@ -99,14 +18,6 @@ void rxPacketTimeout(uintptr_t context, uint32_t currTick) {
 //	DEBUG("%s()\n", __func__);
 //	PLIB_INT_SourceFlagClear(INT_ID_0, INT_SOURCE_USART_1_RECEIVE);
 //}
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Local Functions
-// *****************************************************************************
-// *****************************************************************************
-/* TODO:  Add any necessary local functions.
- */
 
 void restartApp(const char *reason) {
 	SYS_DEBUG_PRINT(SYS_ERROR_WARNING, "Resetting BLEAPP. Reason: %s", reason);
@@ -160,18 +71,6 @@ void stopPacketGuard() {
 	SYS_TMR_CallbackStop(bleappData.packetTimeout);
 	bleappData.packetTimeout = SYS_TMR_HANDLE_INVALID; // FIXME why I should make it invalid? is there a function that do it for me?
 }
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Initialization and State Machine Functions
-// *****************************************************************************
-// *****************************************************************************
-/*******************************************************************************
- Function:
- void BLEAPP_Initialize ( void )
-
- Remarks:
- See prototype in bleapp.h.
- */
 
 void BLEAPP_Initialize(void) {
 	/* Place the App state machine in its initial state. */
@@ -186,13 +85,7 @@ void BLEAPP_Initialize(void) {
 	bleappData.packetHandler = DRV_USART_BUFFER_HANDLE_INVALID;
 	bleappData.packetTimeout = SYS_TMR_HANDLE_INVALID;
 }
-/******************************************************************************
- Function:
- void BLEAPP_Tasks ( void )
 
- Remarks:
- See prototype in bleapp.h.
- */
 uint8_t dummyData[5] = {5, 4, 56, 2, 77};
 void BLEAPP_Tasks(void) {
 
@@ -327,6 +220,7 @@ void BLEAPP_Tasks(void) {
 
             case BLE_CMD_MODE:
                 // TODO forward to slave
+                DEBUG("Command mode received");
                 
                 break;
 
@@ -375,6 +269,3 @@ void BLEAPP_Tasks(void) {
 	}
 	}
 }
-/*******************************************************************************
- End of File
- */
