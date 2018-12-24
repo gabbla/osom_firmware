ecf8191c5d587c3e60383e7f687d803be115c942
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-24 21:32:27 +0100

    added ref to BLE_OUT

--- firmware/src/mailbox.h
+++ firmware/src/mailbox.h
@@ -7,14 +7,15 @@
 #define MSG_SRC_MAIN            (1)
 #define MSG_SRC_COM             (2)
 
-// LASER
-#define LASER_MAILBOX           (SYS_MSG_0)
-#define LASER_MSG_PRIORITY      (0)
-#define LASER_MSG_ID            (0)
+// BLE Outgoing
+#define BLEOUT_MAILBOX          (SYS_MSG_0)
+#define BLEOUT_MSG_PRIORITY     (0)
+#define BLEOUT_MSG_ID           (0)
 
-// LASER
+// MAIN
 #define MAIN_MAILBOX           (SYS_MSG_0)
 #define MAIN_MSG_PRIORITY      (0)
 #define MAIN_MSG_ID            (1)
+
 
 #endif /* end of include guard: MAILBOX_H_VY2CRLWE */
