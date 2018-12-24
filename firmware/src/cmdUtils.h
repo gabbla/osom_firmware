ad74f3c579292f146c750060cf57dc783a25d4a4
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-24 23:52:24 +0100

    added header with command parsing utils

---
+++ firmware/src/cmdUtils.h
@@ -0,0 +1,16 @@
+#ifndef CMDUTILS_H_PAFSTZ6W
+#define CMDUTILS_H_PAFSTZ6W
+
+// BLE_CMD_MODE
+#define MODE_LASER_MASK     (0x0F)
+
+inline uint8_t MODE_GetMode(const uint8_t *payload) {
+    return payload[0] >> 4;
+}
+
+inline uint8_t MODE_GetLaser(const uint8_t *payload) {
+    return payload[0] & MODE_LASER_MASK;
+}
+
+
+#endif /* end of include guard: CMDUTILS_H_PAFSTZ6W */
