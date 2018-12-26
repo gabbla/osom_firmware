e77171603bb341202bda0679c077b51878643fad
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-26 23:42:24 +0100

    cleanup

--- firmware/src/bleApp.h
+++ firmware/src/bleApp.h
@@ -31,6 +31,8 @@ extern "C" {
 #define MAX_PACKET_IN_QUEUE_OUT	10
 #define PACKET_RX_TIMEOUT		1000 // ms
 
+#define INVALID_MESSAGE_ID      (0)
+
 typedef enum
 {
 	/* Application's state machine's initial state. */
@@ -68,83 +70,8 @@ typedef struct
 } BLEAPP_DATA;
 
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Callback Routines
-// *****************************************************************************
-// *****************************************************************************
-/* These routines are called by drivers when certain events occur.
-*/
-	
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Initialization and State Machine Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/*******************************************************************************
-  Function:
-    void BLEAPP_Initialize ( void )
-
-  Summary:
-     MPLAB Harmony application initialization routine.
-
-  Description:
-    This function initializes the Harmony application.  It places the 
-    application in its initial state and prepares it to run so that its 
-    APP_Tasks function can be called.
-
-  Precondition:
-    All other system initialization routines should be called before calling
-    this routine (in "SYS_Initialize").
-
-  Parameters:
-    None.
-
-  Returns:
-    None.
-
-  Example:
-    <code>
-    BLEAPP_Initialize();
-    </code>
-
-  Remarks:
-    This routine must be called from the SYS_Initialize function.
-*/
-
 void BLEAPP_Initialize ( void );
 
-
-/*******************************************************************************
-  Function:
-    void BLEAPP_Tasks ( void )
-
-  Summary:
-    MPLAB Harmony Demo application tasks function
-
-  Description:
-    This routine is the Harmony Demo application's tasks function.  It
-    defines the application's state machine and core logic.
-
-  Precondition:
-    The system and application initialization ("SYS_Initialize") should be
-    called before calling this.
-
-  Parameters:
-    None.
-
-  Returns:
-    None.
-
-  Example:
-    <code>
-    BLEAPP_Tasks();
-    </code>
-
-  Remarks:
-    This routine must be called from SYS_Tasks() routine.
- */
 
 void BLEAPP_Tasks( void );
 
