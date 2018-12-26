6f73c4e70c5461d2419f71f1964d3204f3bf6d56
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2018-12-26 23:04:44 +0100

    RPB4 mapped as input

--- firmware/src/system_config/Debug/system_config.h
+++ firmware/src/system_config/Debug/system_config.h
@@ -96,7 +96,7 @@ extern "C" {
 #define SYS_PORT_A_CNPD         0x0000
 #define SYS_PORT_A_CNEN         0x0000
 
-#define SYS_PORT_B_ANSEL        0xF010
+#define SYS_PORT_B_ANSEL        0xF000
 #define SYS_PORT_B_TRIS         0xFFFF
 #define SYS_PORT_B_LAT          0x0000
 #define SYS_PORT_B_ODC          0x0000
