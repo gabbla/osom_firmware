e4dc71cf43cf6e249350f62269b7a4c914867afe
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-01-05 20:22:32 +0100

    added a missing include

--- firmware/src/logger.h
+++ firmware/src/logger.h
@@ -8,6 +8,8 @@
 #ifndef FIRMWARE_SRC_LOGGER_H_
 #define FIRMWARE_SRC_LOGGER_H_
 
+#include "system/debug/sys_debug.h"
+
 #define DEBUG(fmt, ...)	SYS_DEBUG_PRINT(SYS_ERROR_DEBUG, 	"[DEBUG] " fmt "\n", ##__VA_ARGS__)
 #define INFO(fmt, ...)	SYS_DEBUG_PRINT(SYS_ERROR_INFO, 	"[INFO ] " fmt "\n", ##__VA_ARGS__)
 #define WARN(fmt, ...)	SYS_DEBUG_PRINT(SYS_ERROR_WARNING, 	"[WARN ] " fmt "\n", ##__VA_ARGS__)
