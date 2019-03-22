577b97e0d658ef2a57ed411305f56510ea6fc41f
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-03-22 22:06:25 +0100

    i2c works

--- firmware/src/powerapp.c
+++ firmware/src/powerapp.c
@@ -20,8 +20,8 @@ void POWERAPP_Initialize(void) { powerappData.state = POWERAPP_STATE_INIT; }
     See prototype in powerapp.h.
  */
 
-uint8_t voltage[] = {4, 5};
-            uint8_t received[2];
+uint8_t voltage[] = {4, 5, 0};
+uint8_t received[] = {0, 0 , 0};
 
 void POWERAPP_Tasks(void) {
     /* Check the application's current state. */
@@ -30,15 +30,15 @@ void POWERAPP_Tasks(void) {
         case POWERAPP_STATE_INIT: {
             bool appInitialized = true;
             powerappData.gauge =
-                DRV_I2C_Open(DRV_I2C_INDEX_0,
-                             DRV_IO_INTENT_BLOCKING | DRV_IO_INTENT_READWRITE);
-            if(powerappData.gauge == DRV_HANDLE_INVALID) {
+                DRV_I2C_Open(DRV_I2C_INDEX_0, DRV_IO_INTENT_READWRITE);
+            if (powerappData.gauge == DRV_HANDLE_INVALID) {
                 ERROR("Gauge I2C handler is invalid");
                 appInitialized = false;
             }
-            
-            DRV_I2C_TransmitThenReceive(powerappData.gauge, GAUGE_ADDRESS,
-                                        voltage, 2, received, 2, NULL);
+
+            DRV_I2C_BUFFER_HANDLE hBuff =
+                DRV_I2C_TransmitThenReceive(powerappData.gauge, 0xAA,
+                                            voltage, 2, received, 2, NULL);
             asm("nop");
             if (appInitialized) {
                 powerappData.state = POWERAPP_STATE_SERVICE_TASKS;
