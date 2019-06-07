fb48b3a8bef34db0f8d1a722b4970a69b64560a9
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-06-08 01:24:27 +0200

    nrf_ifc: added APIs

--- firmware/src/nrf_ifc.c
+++ firmware/src/nrf_ifc.c
@@ -1,16 +1,21 @@
 #include "nrf_ifc.h"
 
-
-NRF_Status nrf_write_register(const uint8_t reg, const uint8_t value) {
+NRF_Status nrf_write_register_size(const uint8_t reg, const uint8_t *data, const size_t len) {
     //DEBUG("%s() reg: 0x%02X value: 0x%02X", __func__, reg, value);
     PLIB_SPI_BufferClear(NRF_SPI_ID);
     spi_active();
     PLIB_SPI_BufferWrite(NRF_SPI_ID, W_REGISTER | (REGISTER_MASK & reg));
-    PLIB_SPI_BufferWrite(NRF_SPI_ID, value);
+    size_t i;
+    for(i = 0; i < len; ++i)
+        PLIB_SPI_BufferWrite(NRF_SPI_ID, data[i]);
     while (!PLIB_SPI_FIFOShiftRegisterIsEmpty(NRF_SPI_ID))
         ; // Wait until the shift register is empty
     spi_idle();
     return (NRF_Status)PLIB_SPI_BufferRead(NRF_SPI_ID);
+}
+
+NRF_Status nrf_write_register(const uint8_t reg, const uint8_t value) {
+    return nrf_write_register_size(reg, &value, 1);
 }
 
 NRF_Status nrf_read_register(const uint8_t reg, uint8_t *value) {
@@ -42,6 +47,8 @@ NRF_Status nrf_send_command(const uint8_t cmd) {
 NRF_Status NRF_Initialize() {
     // Power down the module
     NRF_PowerEnable(false);
+    // Enable irq reflection (default)
+    NRF_EnableInterrupts(RX_DR | TX_DS | MAX_RT);
     // Clear interrupts
     NRF_CleanInterrupts(RX_DR | TX_DS | MAX_RT);
     // Flush pipes
@@ -75,6 +82,57 @@ NRF_Status NRF_CleanInterrupts(const uint8_t int_mask) {
         status |= (1<< MAX_RT);
     return nrf_write_register(STATUS, status);
 }
+
+NRF_Status NRF_EnableInterrupts(const uint8_t int_mask) {
+    uint8_t config;
+    nrf_read_register(CONFIG, &config);
+    if(int_mask & RX_DR)
+        config &= ~(1 << MASK_RX_DR);
+    if(int_mask & MASK_TX_DS)
+        config &= ~(1 << TX_DS);
+    if(int_mask & MASK_MAX_RT)
+        config &= ~(1<< MAX_RT);
+    return nrf_write_register(CONFIG, config);
+}
+
+NRF_Status NRF_DisableInterrupts(const uint8_t int_mask) {
+    uint8_t config;
+    nrf_read_register(CONFIG, &config);
+    if(int_mask & RX_DR)
+        config |= (1 << MASK_RX_DR);
+    if(int_mask & MASK_TX_DS)
+        config |= (1 << TX_DS);
+    if(int_mask & MASK_MAX_RT)
+        config |= (1<< MAX_RT);
+    return nrf_write_register(CONFIG, config);
+}
+
+NRF_Status NRF_SetAddressWidth(const uint8_t aw) {
+    // No need to read SETUP_AW
+    return nrf_write_register(SETUP_AW, aw);
+}
+
+NRF_Status NRF_SetPipe0Address(const uint8_t *addr, const size_t len) {
+    // TODO check on AW?
+    return nrf_write_register_size(RX_ADDR_P0, addr, len);
+}
+
+NRF_Status NRF_SetPipeBaseAddress(const uint8_t *addr, const size_t len) {
+    // TODO check on AW?
+    return nrf_write_register_size(RX_ADDR_P1, addr, len);
+}
+
+NRF_Status NRF_SetPipeXAddress(const uint8_t pipe, const uint8_t addr) {
+    return nrf_write_register(RX_ADDR_P0 + pipe, addr);
+}
+
+NRF_Status NRF_StartListening() {
+    nrf_set_ce();
+    return nrf_write_register(CONFIG, (1 << PWR_UP) | (1 << PRIM_RX));
+
+}
+
+
 
 NRF_Status NRF_FlushTx() {
     return nrf_send_command(FLUSH_TX);
--- firmware/src/nrf_ifc.h
+++ firmware/src/nrf_ifc.h
@@ -10,6 +10,9 @@
 #define spi_active() PLIB_PORTS_PinClear(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_0); asm("nop")
 #define spi_idle()   PLIB_PORTS_PinSet(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_0); asm("nop")
 
+#define nrf_set_ce() PLIB_PORTS_PinClear(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15); asm("nop")
+#define nrf_clr_ce() PLIB_PORTS_PinSet(PORTS_ID_0, PORT_CHANNEL_B, PORTS_BIT_POS_15); asm("nop")
+
 typedef union {
     struct {
         uint8_t tx_full : 1;
@@ -47,6 +50,54 @@ NRF_Status NRF_PowerEnable(const bool power);
  * @return Chip STATUS register
  */
 NRF_Status NRF_CleanInterrupts(const uint8_t int_mask);
+
+/*
+ * @brief Enable the interrupts reflection on IRQ pin
+ * @param int_mask Any ORed combination of RX_DR,_TX_DS and MAX_RT
+ * @return Chip STATUS register
+ */
+NRF_Status NRF_EnableInterrupts(const uint8_t int_mask);
+
+/*
+ * @brief Disable the interrupts reflection on IRQ pin
+ * @param int_mask Any ORed combination of RX_DR,_TX_DS and MAX_RT
+ * @return Chip STATUS register
+ */
+NRF_Status NRF_DisableInterrupts(const uint8_t int_mask);
+
+/*
+ * @brief Setup the address widthsm common for all data pipes
+ * @param aw AW_x_BYTES where x = {3, 4, 5}. Cannot be ORed
+ * @return Chip STATUS register
+ */
+NRF_Status NRF_SetAddressWidth(const uint8_t aw);
+
+/*
+ * @brief Set the pipe0 address. Write max AW bytes
+ * @param addr vector filled with address, LSB first
+ * @oaram len address length
+ * @return Chip STATUS register
+ */
+NRF_Status NRF_SetPipe0Address(const uint8_t *addr, const size_t len);
+
+/*
+ * @brief Set the pipe 1-5 base address. Write max AW bytes.
+ * @param addr vector filled with address, LSB first
+ * @oaram len address length
+ * @return Chip STATUS register
+ */
+NRF_Status NRF_SetPipeBaseAddress(const uint8_t *addr, const size_t len);
+
+/*
+ * @brief Set the pipe 1-5 LSB
+ * @param pipe Index from 1 to 5
+ * @param addr byte addr to set to pipe
+ * @return Chip STATUS register
+ */
+NRF_Status NRF_SetPipeXAddress(const uint8_t pipe, const uint8_t addr);
+
+NRF_Status NRF_StartListening();
+
 
 /*
  * @brief Send the command FLUSH_TX to the chip
