d62a4389df3ce32bae601ea62745df388c31202c
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-09-21 22:56:21 +0200

    somconfig: added

---
+++ firmware/src/somconfig.c
@@ -0,0 +1,37 @@
+#include "somconfig.h"
+
+static const SOMConfig defconfig = {
+    .offset = SOM_CFG_ADDRESS,
+    .version = SOM_CFG_VERSION,
+    .name = "SOM_TestBoard\0",
+    .flags = {
+        .role = SOM_CFG_ROLE_MASTER
+    }
+};
+
+bool CFG_IsValid(SOMConfig *cfg) {
+    if(!cfg)
+        return false;
+#ifdef SOM_CFG_FORCE_OVERWRITE
+    // To adoid recursion
+    static bool called = false;
+    if(!called) {
+        called = true;
+        return false;
+    }
+    return true;
+#else
+    return cfg->version == SOM_CFG_VERSION;
+#endif
+}
+
+
+void CFG_SetDefault(SOMConfig *cfg) {
+    memcpy(cfg, &defconfig, sizeof(SOMConfig));
+}
+
+bool CFG_IsMaster(SOMConfig *cfg) {
+    if(!cfg)
+        return false;
+    return cfg->flags.role == SOM_CFG_ROLE_MASTER;
+}
---
+++ firmware/src/somconfig.h
@@ -0,0 +1,43 @@
+#ifndef SOMCONFIG_H_V1ILYIPR
+#define SOMCONFIG_H_V1ILYIPR
+
+#include <stdint.h>
+#include "logger.h"
+
+// Every time a field is added to SOMConfig the version must be incremented
+#define SOM_CFG_VERSION (0x00)
+
+// Define to fource config overwrite (useful during debug)
+#define SOM_CFG_FORCE_OVERWRITE
+
+// Address in eeprom
+#define SOM_CFG_ADDRESS 0x0000
+// Max name char
+#define SOM_CFG_NAME_LEN_MAX        14
+// Roles
+#define SOM_CFG_ROLE_MASTER         1
+#define SOM_CFG_ROLE_SLAVE          0
+typedef struct {
+    // offset in memory
+    uint16_t offset;
+    // Configuration version
+    uint8_t version;
+    uint8_t name[SOM_CFG_NAME_LEN_MAX];
+    
+    union {
+        struct {
+            unsigned role : 1; // 1 if master, 0 if slave
+            unsigned slave_id : 3; // 0..4 unused in master mode
+            uint8_t dummy : 4;
+        };
+        uint8_t value;
+    } flags;
+} SOMConfig;
+
+bool CFG_IsValid(SOMConfig *cfg);
+
+void CFG_SetDefault(SOMConfig *cfg);
+
+bool CFG_IsMaster(SOMConfig *cfg);
+
+#endif /* end of include guard: SOMCONFIG_H_V1ILYIPR */
