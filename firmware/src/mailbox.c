e111ff0fe5026bbbc6d86dc1542fa83e9026abdb
Gabriele Zampieri <zampierigabriele.ist@gmail.com>
2019-09-22 18:18:50 +0200

    mailbox: added helpers

--- firmware/src/mailbox.c
+++ firmware/src/mailbox.c
@@ -1,13 +1,27 @@
 #include "mailbox.h"
 
 
+int8_t sendTo(uint8_t src, uint8_t dst, Packet *packet);
+
 int8_t SendPacketToBle(uint8_t src, Packet *packet) {
+    return sendTo(src, BLEOUT_MSG_ID, packet);
+}
+
+int8_t SendPacketToMainapp(uint8_t src, Packet *packet) {
+    return sendTo(src, MAIN_MSG_ID, packet);
+}
+
+int8_t SendPacketToNrfApp(uint8_t src, Packet *packet) {
+    return sendTo(src, NRF_MSG_ID, packet);
+}
+
+int8_t sendTo(uint8_t src, uint8_t dst, Packet *packet) {
     SYS_MSG_OBJECT message;
-    message.nMessageTypeID = BLEOUT_MSG_ID;
+    message.nMessageTypeID = dst;
     message.nSource = src;
     message.nSizeData = sizeof(Packet);
     message.pData = (uintptr_t*)packet;
-    SYS_MSG_RESULTS myRes = SYS_MSG_MessageSend(BLEOUT_MAILBOX, &message);
+    SYS_MSG_RESULTS myRes = SYS_MSG_MessageSend(SYS_MSG_0, &message);
     if(myRes != SYS_MSG_SENT) {
         ERROR("Failed to send!! %d", myRes);
         return -1;
--- firmware/src/mailbox.h
+++ firmware/src/mailbox.h
@@ -10,6 +10,14 @@
 #define MSG_SRC_MAIN            (1)
 #define MSG_SRC_NRF             (2)
 
+static const char *srcName[3]= {
+    "MSG_SRC_BLE",
+    "MSG_SRC_MAIN",
+    "MSG_SRC_NRF"
+};
+
+#define MAILBOX_SRC_TO_STR(x) (srcName[x])
+
 // BLE Outgoing
 #define BLEOUT_MAILBOX          (SYS_MSG_0)
 #define BLEOUT_MSG_PRIORITY     (0)
@@ -28,5 +36,5 @@
 // Utils
 
 int8_t SendPacketToBle(uint8_t src, Packet *packet);
-
+int8_t SendPacketToMainapp(uint8_t src, Packet *packet);
 #endif /* end of include guard: MAILBOX_H_VY2CRLWE */
